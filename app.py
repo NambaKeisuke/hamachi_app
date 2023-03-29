64c29277fbd9d1303d15aac402549031bca3c34d
NKeistyle <chocopalv@gmail.com>
2023-03-29 14:48:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -99,6 +99,14 @@ def graph(forecast_range):
     go.Scatter(x=test_pred.index, y=test_pred['predicted_mean'], name='予測'),
     go.Scatter(x=test_pred_max.index, y=test_pred_max['predicted_mean'], name='大量予想日', marker={'size': 10, 'symbol': 'star', 'color':'gold'}, ),
     ])
+
+    fig.update_layout(title=dict(text='ハマチの卸売数量予測結果',
+                             x=0.5,
+                             y=0.9,
+                             xanchor='center'
+                            ),
+                    xaxis=dict(title='年月日'),
+                    yaxis=dict(title='ハマチの卸売数量(kg)'))
     return fig
 
 def text():
@@ -122,10 +130,10 @@ with gr.Blocks() as iface:
         """
         ## 本アプリケーションについて
         - 本アプリケーションは現在から選択された日数までの期間における、豊洲市場におけるハマチの卸売数量の推移をS-ARIMAモデルを用いて予測し、卸売数量が最も多くなると予想される日を表示するアプリケーションです
+        - 本アプリケーションではユーザーがアクセスしたタイミングでデータを自動更新しております
         - 本アプリケーションは以下のデータを用いて豊洲市場におけるハマチの卸売数量を予測しています
             - 東京卸売市場日報 (https://www.shijou-nippo.metro.tokyo.lg.jp/SN/SN_Sui_Nengetu.html)
             - 小売物価統計調査 (https://www.stat.go.jp/data/kouri/doukou/3.html)
-            - 各種データ・資料 - 気象庁 (https://www.jma.go.jp/jma/menu/menureport.html)
             - 市場開場日・休業日年間カレンダー - 東京都中央卸売市場 (https://www.shijou.metro.tokyo.lg.jp/calendar/)
         """
     )
